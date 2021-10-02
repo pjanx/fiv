f21100fb02e219dc80fce7211c1f86174d4b1c03
Přemysl Eric Janouch <p@janouch.name>
2021-10-02 07:06:42 +0200

    clean-up-thumbnail-cache.sh: local file URIs only

--- tools/clean-up-thumbnail-cache.sh
+++ tools/clean-up-thumbnail-cache.sh
@@ -6,7 +6,7 @@ pnginfo=$(pwd)/pnginfo cache_home=${XDG_CACHE_HOME:-$HOME/.cache}
 for size in normal large x-large xx-large; do
 	cd "$cache_home/thumbnails/$size" 2>/dev/null || continue
 	find . -name '*.png' -print0 | PNGINFO_SKIP_TRAILING=1 xargs -0 "$pnginfo" \
-		| jq -r '.info.texts."Thumb::URI"' | grep '^file://' \
+		| jq -r '.info.texts."Thumb::URI"' | grep '^file:///' \
 		| grep -v '^file:///run/media/[^/]*/NIKON/' \
 		| perl -MURI -MURI::Escape -MDigest::MD5 -lne \
 			'print Digest::MD5->new()->add($_)->hexdigest . ".png"
