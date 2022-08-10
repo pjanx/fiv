cf19f8287547c523f65879b9e9477950c81a9eed
Přemysl Eric Janouch <p@janouch.name>
2022-08-10 18:11:05 +0200

    Automate Windows builds, add icons to executables
    Scripts have been ported from sdtui, and adjusted for Meson.
    
    The port is broken through and through on WINE,
    but sort-of works natively.

---
+++ msys2-cross-configure.sh
@@ -0,0 +1,111 @@
+#!/bin/sh -e
+# msys2-cross-configure.sh: set up an MSYS2-based cross-compiled Meson build.
+# Dependencies: AWK, sed, sha256sum, cURL, bsdtar,
+# wine64, Meson, mingw-w64-binutils, mingw-w64-gcc, pkg-config
+repository=https://repo.msys2.org/mingw/mingw64/
+
+status() {
+	echo "$(tput bold)-- $*$(tput sgr0)"
+}
+
+dbsync() {
+	status Fetching repository DB
+	[ -f db.tsv ] || curl -# "$repository/mingw64.db" | bsdtar -xOf- | awk '
+		function flush() { print f["%NAME%"] f["%FILENAME%"] f["%DEPENDS%"] }
+		NR > 1 && $0 == "%FILENAME%" { flush(); for (i in f) delete f[i] }
+		!/^[^%]/ { field = $0; next } { f[field] = f[field] $0 "\t" }
+		field == "%SHA256SUM%" { path = "*packages/" f["%FILENAME%"]
+			sub(/\t$/, "", path); print $0, path > "db.sums" } END { flush() }
+	' > db.tsv
+}
+
+fetch() {
+	status Resolving "$@"
+	mkdir -p packages
+	awk -F'\t' 'function get(name,    i, a) {
+		if (visited[name]++ || !(name in filenames)) return
+		print filenames[name]; split(deps[name], a); for (i in a) get(a[i])
+	} BEGIN { while ((getline < "db.tsv") > 0) {
+		filenames[$1] = $2; deps[$1] = ""; for (i = 3; i <= NF; i++) {
+			gsub(/[<=>].*/, "", $i); deps[$1] = deps[$1] $i FS }
+	} for (i = 0; i < ARGC; i++) get(ARGV[i]) }' "$@" | while IFS= read -r name
+	do
+		status Fetching "$name"
+		[ -f "packages/$name" ] || curl -#o "packages/$name" "$repository/$name"
+	done
+}
+
+verify() {
+	status Verifying checksums
+	sha256sum --ignore-missing --quiet -c db.sums
+}
+
+extract() {
+	status Extracting packages
+	for subdir in *
+	do [ -d "$subdir" -a "$subdir" != packages ] && rm -rf -- "$subdir"
+	done
+	for i in packages/*
+	do bsdtar -xf "$i" --strip-components 1 mingw64
+	done
+}
+
+configure() {
+	# Don't let GLib programs inherit wrong paths from the environment.
+	export XDG_DATA_DIRS=$msys2_root/share
+
+	status Configuring packages
+	wine64 bin/update-mime-database.exe share/mime
+	wine64 bin/glib-compile-schemas.exe share/glib-2.0/schemas
+	wine64 bin/gdk-pixbuf-query-loaders.exe \
+		> lib/gdk-pixbuf-2.0/2.10.0/loaders.cache
+}
+
+setup() {
+	status Setting up Meson
+	cat >"$toolchain" <<-EOF
+	[binaries]
+	c = 'x86_64-w64-mingw32-gcc'
+	cpp = 'x86_64-w64-mingw32-g++'
+	ar = 'x86_64-w64-mingw32-gcc-ar'
+	ranlib = 'x86_64-w64-mingw32-gcc-ranlib'
+	strip = 'x86_64-w64-mingw32-strip'
+	windres = 'x86_64-w64-mingw32-windres'
+	pkgconfig = 'pkg-config'
+
+	[properties]
+	sys_root = '$builddir'
+	msys2_root = '$msys2_root'
+	pkg_config_libdir = '$msys2_root/share/pkgconfig:$msys2_root/lib/pkgconfig'
+	needs_exe_wrapper = true
+
+	[host_machine]
+	system = 'windows'
+	cpu_family = 'x86_64'
+	cpu = 'x86_64'
+	endian = 'little'
+	EOF
+
+	meson --buildtype=debugoptimized --prefix="$packagedir" \
+		--bindir . --libdir . --cross-file="$toolchain" "$builddir" "$sourcedir"
+}
+
+sourcedir=$(realpath "${2:-$(dirname "$0")}")
+builddir=$(realpath "${1:-builddir}")
+packagedir=$builddir/package
+toolchain=$builddir/msys2-cross-toolchain.meson
+
+# This directory name matches the prefix in .pc files, so we don't need to
+# modify them (pkgconf has --prefix-variable, but Meson can't pass that option).
+msys2_root=$builddir/mingw64
+
+mkdir -p "$msys2_root"
+cd "$msys2_root"
+dbsync
+fetch mingw-w64-x86_64-gtk3 mingw-w64-x86_64-lcms2 \
+	mingw-w64-x86_64-libraw mingw-w64-x86_64-libheif \
+	mingw-w64-x86_64-libwinpthread-git # Because we don't do "provides"?
+verify
+extract
+configure
+setup
---
+++ msys2-cross-install.sh
@@ -0,0 +1,65 @@
+#!/bin/sh -e
+export LC_ALL=C
+cd "$MESON_INSTALL_DESTDIR_PREFIX"
+msys2_root=$1
+
+# Copy libraries we depend on, and a few files required by GTK+.
+cp -p "$msys2_root"/bin/*.dll .
+cp -pR "$msys2_root"/etc/ .
+
+mkdir -p lib
+cp -pR "$msys2_root"/lib/gdk-pixbuf-2.0/ lib
+mkdir -p share/glib-2.0
+cp -pR "$msys2_root"/share/glib-2.0/schemas/ share/glib-2.0
+mkdir -p share
+cp -pR "$msys2_root"/share/mime/ share
+mkdir -p share/icons
+cp -pR "$msys2_root"/share/icons/Adwaita/ share/icons
+mkdir -p share/icons/hicolor
+cp -p "$msys2_root"/share/icons/hicolor/index.theme share/icons/hicolor
+
+# Remove unreferenced libraries.
+find lib -name '*.a' -exec rm -- {} +
+awk 'function whitelist(binary) {
+	if (seen[binary]++)
+		return
+
+	delete orphans[binary]
+	while (("strings -a \"" binary "\" 2>/dev/null" | getline string) > 0)
+		if (match(string, /[-.+_a-zA-Z0-9]+[.][Dd][Ll][Ll]$/))
+			whitelist("./" substr(string, RSTART, RLENGTH))
+} BEGIN {
+	while (("find . -type f -path \"./*.[Dd][Ll][Ll]\"" | getline) > 0)
+		orphans[$0]++
+	while (("find . -type f -path \"./*.[Ee][Xx][Ee]\"" | getline) > 0)
+		whitelist($0)
+	while (("find ./lib/gdk-pixbuf-2.0 -type f " \
+		"-path \"./*.[Dd][Ll][Ll]\"" | getline) > 0)
+		whitelist($0)
+	for (library in orphans)
+		print library
+}' | xargs rm --
+
+# Removes unused icons from the Adwaita theme. It could be even more aggressive,
+# since it keeps around lots of sizes and all the GTK+ stock icons.
+find share/icons/Adwaita -type f | awk 'BEGIN {
+	while (("grep -aho \"[a-z][a-z-]*\" *.dll *.exe" | getline) > 0)
+		good[$0] = 1
+} /[.](png|svg|cur|ani)$/ {
+	# Cut out the basename without extensions.
+	match($0, /[^\/]+$/)
+	base = substr($0, RSTART)
+	sub(/[.].+$/, "", base)
+
+	# Try matching while cutting off suffixes.
+	# Disregarding the not-much-used GTK_ICON_LOOKUP_GENERIC_FALLBACK.
+	while (!(keep = good[base]) &&
+		sub(/-(ltr|rtl|symbolic)$/, "", base)) {}
+	if (!keep)
+		print
+}' | xargs rm --
+
+wine64 "$msys2_root"/bin/glib-compile-schemas.exe share/glib-2.0/schemas
+
+# This may speed up program start-up a little bit.
+wine64 "$msys2_root"/bin/gtk-update-icon-cache-3.0.exe share/icons/Adwaita
