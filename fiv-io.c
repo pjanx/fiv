c063d93dc7037578a475ae559478bd302358472f
Přemysl Eric Janouch <p@janouch.name>
2022-12-23 17:03:50 +0100

    Fix LibRaw 0.21.0 build

--- fiv-io.c
+++ fiv-io.c
@@ -1697,6 +1697,11 @@ fail:
 
 #ifdef HAVE_LIBRAW  // ---------------------------------------------------------
 
+// LibRaw 0.21.0 compatibility.
+#ifndef LIBRAW_OPIONS_NO_MEMERR_CALLBACK
+#define LIBRAW_OPIONS_NO_MEMERR_CALLBACK 0
+#endif
+
 static cairo_surface_t *
 open_libraw(const char *data, gsize len, GError **error)
 {
